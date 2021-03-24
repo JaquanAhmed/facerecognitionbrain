7759ca431a9bcbd0b4349b99828390a6aa5cf819
JaquanAhmed <jaq-ahmed@hotmail.co.uk>
2021-03-24 00:03:49 +0000

    Updates

---
+++ precache-manifest.6c0e5bbe98512fc91108824ca47c8501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02aaf19a6d6f747d6b011a2c5ea16f52",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "a3351d01c4b3891bb5d8",
+    "url": "/facerecognitionbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1073b6b458b9327112aa",
+    "url": "/facerecognitionbrain/static/css/main.594cc2e6.chunk.css"
+  },
+  {
+    "revision": "a3351d01c4b3891bb5d8",
+    "url": "/facerecognitionbrain/static/js/2.31448a78.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerecognitionbrain/static/js/2.31448a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1073b6b458b9327112aa",
+    "url": "/facerecognitionbrain/static/js/main.617750d3.chunk.js"
+  },
+  {
+    "revision": "466a2f4fa9e4937c0bc5",
+    "url": "/facerecognitionbrain/static/js/runtime-main.a824e37d.js"
+  }
+]);
